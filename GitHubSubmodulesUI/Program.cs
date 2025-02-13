c55c25a32791165a0fc24ce925a9c295c23f69f7
Paulo Neto <31972103+netiim@users.noreply.github.com>
2025-02-13 07:56:04 -0300

    teste ajustes

--- GitHubSubmodulesUI/Program.cs
+++ GitHubSubmodulesUI/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Framework.Services;
+
+PessoaService p = new PessoaService();
+Console.WriteLine(p.ExibirDetalhes(new Framework.Domain.Pessoa()
+{
+    Nome = "Paulo",
+    Idade = 15,
+    Altura = 1.65
+})  );  
